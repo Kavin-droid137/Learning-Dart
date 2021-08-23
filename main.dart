d6c2ad4f348156b1925e3a8805755db3724320bb
Kavin Ganesh R <77374022+Kavin-droid137@users.noreply.github.com>
2021-08-23 15:44:40 +0000

    Variables

--- main.dart
+++ main.dart
@@ -10,4 +10,14 @@ void main() {
     print("  /    |");
     print(" /     |");
     print("/______|");
+
+    //Chapter 3: Variables
+    String ProgrammingLanguageName = "Flutter";
+    String ProgrammingLanguageAge = "5";
+    print("There was a framework called ${ProgrammingLanguageName}");
+    print("He was ${ProgrammingLanguageAge} years old");
+    ProgrammingLanguageName = "Dart";
+    ProgrammingLanguageAge = "7";
+    print("He really liked his name ${ProgrammingLanguageName}");
+    print("But didn't like being ${ProgrammingLanguageAge} years old");
 }
